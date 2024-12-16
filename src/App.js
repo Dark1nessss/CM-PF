6adbbf7f5ffbf5826abc56cab451658b7c9d43af
Dark1nessss <frankhorn99@gmail.com>
2024-12-16 18:03:41 +0000

    Starting Files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Lista de Tarefas 🚀</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
